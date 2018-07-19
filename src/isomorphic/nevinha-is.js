9fed4d3becb9c441bc2dfdb9ab9c3267732e78dc
Clóvis Neto <cloves13@hotmail.com>
2018-07-19 03:03:51 -0300

    Sf

--- src/isomorphic/nevinha-is.js
+++ src/isomorphic/nevinha-is.js
@@ -2,6 +2,8 @@ import {definedMotionsProps} from '../motions/index';
 
 export const isClass = func => {
   return func.prototype.render;
+};
+
 export const isPXUnit = value => {
   return /px$/.test(value);
 };
