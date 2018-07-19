1a647ab26532a319f7618da8e167924a1d103414
Clóvis Neto <cloves13@hotmail.com>
2018-07-19 03:05:56 -0300

    Add nevinha-is methods in the export of isomorphic structure

--- src/isomorphic/index.js
+++ src/isomorphic/index.js
@@ -1,10 +1,14 @@
 import {IsomorphicNevinhaComponent} from './component';
 import {updateElement, changed} from './diff';
 import {createVirtualElement} from './render';
+import {isClass, isCustomProp, hasCustomProp} from './nevinha-is';
 
 export {
   changed,
   createVirtualElement,
+  hasCustomProp,
   IsomorphicNevinhaComponent,
+  isClass,
+  isCustomProp,
   updateElement
 };
