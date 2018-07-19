7df176e3af2fc5642abc5f4630cf91bb1fa794d7
Clóvis Neto <cloves13@hotmail.com>
2018-07-19 03:01:00 -0300

    Create motion Architecture

--- src/motions/dom/motions-props.js
+++ src/motions/dom/motions-props.js
@@ -1,6 +1,113 @@
-export const customMotionsProps = {
-  sticker: {
-    values: ['sticky'],
-    speed: 0.5
+import {setTypedStyle, setTransition} from '../providers/CSSProvider';
+import {requestTimeout} from '../providers/animations';
+import {isNumber} from '../../isomorphic/nevinha-is';
+
+const STICKY_MOTION = 'sticky';
+const FADE_OUT_MOTION = 'fadeOut';
+const FADE_IN_MOTION = 'fadeOut';
+
+export const setStickyPosition = ($el, values) => {
+  setTypedStyle($el, 'position', STICKY_MOTION);
+  setTypedStyle($el, 'top', getValue(values, 'top', STICKY_MOTION));
+};
+
+export const setFadeOut = ($el, values) => {
+  const speed = getValue(values, 'speed', FADE_OUT_MOTION);
+  const timingFn = getValue(values, 'timingFn', FADE_IN_MOTION);
+
+  requestTimeout(()=> setTypedStyle($el, 'display', 'none'), speed * 1000);
+
+  setTransition($el, 'opacity', speed, timingFn);
+
+  requestTimeout(() =>  setTypedStyle($el, 'opacity', 0));
+};
+
+export const setFadeIn = ($el, values) => {
+  const speed = getValue(values, 'speed', FADE_IN_MOTION);
+  const timingFn = getValue(values, 'timingFn', FADE_IN_MOTION);
+
+  setTypedStyle($el, 'display', 'inherit');
+  setTypedStyle($el, 'opacity', 0);
+  setTransition($el, 'opacity', speed, timingFn);
+
+  requestTimeout(()=> setTypedStyle($el, 'opacity', 1), 0);
+};
+
+export const setHide = ($el) => {
+  setTypedStyle($el, 'display', 'none');
+};
+
+export const setShow = ($el) => {
+  setTypedStyle($el, 'display', 'inherit');
+};
+
+export const definedMotionsProps = {
+  fadeIn: {
+    callFn: setFadeIn,
+    values: {
+      speed: {
+        defaultValue: 0.5,
+        rulesFn: () => [isNumber]
+      },
+      timingFn: {
+        defaultValue: 'ease-out'
+      }
+    }
+  },
+  fadeOut: {
+    callFn: setFadeOut,
+    values: {
+      speed: {
+        defaultValue: 1,
+        rulesFn: () => [isNumber]
+      },
+      timingFn: {
+        defaultValue: 'ease-out'
+      }
+    }
+  },
+  hide: {
+    callFn: setHide
+  },
+  show: {
+    callFn: setShow
+  },
+  sticky: {
+    callFn: setStickyPosition,
+    values: {
+      top: {
+        defaultValue: '0px'
+      }
+    }
   }
+};
+
+export const getDefaultValues = (values, motionName) => {
+  if (typeof values !== 'object')
+    return definedMotionsProps[motionName].values;
+
+  return values;
+};
+
+export const getValue = (valueConfig, property, motionName) => {
+  const propertyConfig = getDefaultValues(true, motionName)[property];
+  const userValue = valueConfig[property];
+
+  if (!userValue) return propertyConfig.defaultValue;
+
+  if (propertyConfig.rulesFn) {
+    propertyConfig.rulesFn().forEach(rule => {
+      const isValid = rule(userValue);
+
+      if (!isValid)
+        throw new Error(
+          `Invalid CSS value parsed to ${motionName} API. For more details, please read more about '${
+            rule.name
+          }' function validation in NevinhaJS API`,
+          rule
+        );
+    });
+  }
+
+  return userValue;
 };
