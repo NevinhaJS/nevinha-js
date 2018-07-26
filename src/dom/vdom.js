ee91b0acad0176b04e045e79a3b7439bec44f875
Clóvis Neto <cloves13@hotmail.com>
2018-07-26 18:29:04 -0300

    Add vdom file for allowing the user uses this method in babel-presets

---
+++ src/dom/vdom.js
@@ -0,0 +1,19 @@
+/**
+ * Generates a virtual DOM representation as a plane Object
+ *
+ * @param {(string|function)} type - Element type
+ * @param {object} attributes - All Props and values
+ * @param {...(string|vdom|array)} ...args -  Child elements
+ *
+ * @returns {object} Virtual DOM representation as a plane object
+ *
+ */
+/*eslint-disable */
+/** @jsx dom */
+const NevinhaDOM = (type, attributes, ...args) => {
+	const children = args.length ? [].concat(...args) : [];
+	return {type, attributes: attributes || {}, children};
+}
+/*eslint-enable */
+
+export default NevinhaDOM;
--- src/nevinha.js
+++ src/nevinha.js
@@ -1,5 +1,7 @@
 import {NevinhaComponent} from './dom/client/client';
 import {render} from './dom/client/render';
+import NevinhaDOM from './dom/vdom';
 import {ParallaxProvider} from './motions/providers/parallaxProvider';
 
 export {NevinhaComponent, render, ParallaxProvider};
+export default NevinhaDOM;
